590c2ad263b7e7bc42d14098cd03b3e60d29026b
Ziyaddin <ziya.huseynov2001@mail.ru>
2024-02-17 14:09:34 +0400

    Добавьте файлы проекта.

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+    public class TicTacToeGame
+    {
+        private readonly State[] board = new State[9];
+        public int MovesCounter { get; private set; }
+
+        public TicTacToeGame()
+        {
+            for(int i=0; i<board.Length; i++)
+            {
+                board[i] = State.Unset;
+            }
+        }
+
+        public void MakeMove(int index)
+        {
+            board[index - 1] = MovesCounter % 2 == 0 ? State.Cross : State.Zero;
+        }
+    }
+}
