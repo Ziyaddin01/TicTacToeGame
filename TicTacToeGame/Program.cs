aed50ed6bdf9ffd857a0b6e2239603e8e9788f3a
Ziyaddin <ziya.huseynov2001@mail.ru>
2024-02-22 19:09:27 +0400

    Final

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -4,71 +4,42 @@ using System.Text;
 
 namespace TicTacToeGame
 {
-    public enum Winner
+    class Program
     {
-        Crosses,
-        Zeroes,
-        Draw,
-        GameIsUnfinished
-    }
-    public enum State
-    {
-        Cross,
-        Zero,
-        Unset
-    }
-    public class TicTacToeGame
-    {
-        private readonly State[] board = new State[9];
-        public int MovesCounter { get; private set; }
-
-        public TicTacToeGame()
+        private static TicTacToeGame g = new TicTacToeGame();
+        static void Main(string[] args)
         {
-            for(int i=0; i<board.Length; i++)
+            Console.WriteLine(GetPrintableState());
+
+            while(g.GetWinner() == Winner.GameIsUnfinished)
             {
-                board[i] = State.Unset;
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
             }
+            Console.WriteLine($"Result: {g.GetWinner()}");
+            Console.ReadLine();
         }
-
-        public void MakeMove(int index)
+        static string GetPrintableState()
         {
-            board[index - 1] = MovesCounter % 2 == 0 ? State.Cross : State.Zero;
-
-            MovesCounter++;
-        }
-
-        public State GetState(int index)
-        {
-            return board[index - 1];
-        }
-        public Winner GetWinner()
-        {
-            return GetWinner(1, 4, 7, 2, 5, 8, 3, 6, 9,
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 
-                1, 5, 9, 3, 5, 7);
-        }
-
-        private Winner GetWinner(params int[] indexes)
-        {
-            for(int i = 0; i<indexes.Length; i += 3)
+            var sb = new StringBuilder();
+            for(int i = 1; i<=7; i += 3)
             {
-                bool same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
-                if(same)
-                {
-                    State state = GetState(indexes[i]);
-                    if (state != State.Unset)
-                    {
-                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
-                    }
-                }
+                sb.AppendLine("     |     |     |")
+                    .AppendLine(
+                    $"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}  |")
+                    .AppendLine("_____|_____|_____|");
             }
-            if (MovesCounter < 9)
-                return Winner.GameIsUnfinished;
-            return Winner.Draw;
+            return sb.ToString();
         }
-        private bool AreSame(int a, int b, int c)
+        static string GetPrintableChar(int index)
         {
-            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+            State state = g.GetState(index);
+            if(state == State.Unset)
+                return index.ToString();
+            return state == State.Cross ? "X" : "O";
         }
     }
 }
